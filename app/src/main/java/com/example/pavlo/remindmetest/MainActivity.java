1d7bb25925cff6f5dda0efe0e63271bedd6aa03a
voanerges14 <pavlozamroz@gmail.com>
2016-08-10 11:53:04 +0300

    First Activity

---
+++ app/src/main/java/com/example/pavlo/remindmetest/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.pavlo.remindmetest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Pavlo on 10-08,Aug-16.
+ */
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
